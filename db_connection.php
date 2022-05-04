75fdea4c83d57b404f51ebdca66652824f1fa964
Tom Yotwongjai <munch2018@gmail.com>
2022-05-04 11:44:48 +0200

    update db config

--- db_connection.php
+++ db_connection.php
@@ -4,14 +4,14 @@
  */
 
  class DbConnect {
-      public $server = 'localhost'; 
-      public $dbname =  'scandi-crud';
-      public $user = 'Tom';
-      public $pass = '';
+      private $DB_HOST = 'remotemysql.com'; 
+      private $DB_NAME =  'D0eR4bv7ru';
+      private $DB_USER = 'D0eR4bv7ru';
+      private $DB_PASSWORD = 'gjW8jmZnMP';
 
       public function connect() {
         try {
-          $conn = new PDO("mysql:host=$this->server;dbname=$this->dbname", $this->user, $this->pass);
+          $conn = new PDO("mysql:host=$this->DB_HOST;dbname=$this->DB_NAME", $this->DB_USER, $this->DB_PASSWORD);
           $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
           return $conn;
         } catch(PDOException $e) {
